6d593ba666cfa3632d8c64e0c050d6bf35d23f74
JP Padilla <jp@JPs-MacBook-Air.local>
2023-05-28 10:14:31 -0700

    added post and get function

--- router/apiRoutes/notes/index.js
+++ router/apiRoutes/notes/index.js
@@ -1,3 +1,29 @@
 const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+
+router.get('/', (req, res) =>{
+    const {notes, title} = req.body;
+    try {
+        fs.readFile(path.join(__dirname, '..', '..', '..', 'db', "notes.js"), 'utf8', (notes, err) => {
+            res.json(notes);
+        });
+    } catch (err) {
+        res.status(400).json(notes, title)
+    }
+});
+
+router.post('/', (req, res) => {
+    const {notes, title} = req.body;
+    try {
+        fs.readFile(path.join(__dirname, '..', '..', '..', 'db'), 'utf8', (notes, err) => {
+            res.json(notes);
+        });
+    } catch (err) {
+        res.status(400).json(notes, title)
+    }
+
+});
 
 module.exports = router;
